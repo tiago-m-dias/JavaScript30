176dd83cc374597a15a023829a844a292a3091a5
Tiago Dias <freshman21@gmail.com>
2019-04-24 00:28:48 +0100

    fix: scrub code review

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -57,7 +57,11 @@ skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
+
+let mousedown = false;
 progress.addEventListener('click', scrub);
-progress.addEventListener('mouseup', scrub);
+progress.addEventListener('mouseup', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
 
 
