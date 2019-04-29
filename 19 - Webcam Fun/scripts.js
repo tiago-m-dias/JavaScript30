83f8c2b04f7ea2c2880132ee4a70967667dfc145
Tiago Dias <freshman21@gmail.com>
2019-04-29 16:52:46 +0100

    feat: webcam in canvas

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,35 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(localMediaStream => {
+        console.log(localMediaStream);
+        video.srcObject = localMediaStream;
+        video.play();
+    }).catch(err => {
+        console.error(`OH NO!!`, err);
+    });
+}
+
+function paintToCanvas() {
+    const {videoWidth: width, videoHeight: height} = video;
+    canvas.width = width;
+    canvas.height = height;
+
+    //get image to canvas every N time
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16);
+}
+
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
