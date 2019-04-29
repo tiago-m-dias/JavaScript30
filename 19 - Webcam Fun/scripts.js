1117c78a20248c6ec92094bc6db76fc2f25c8945
Tiago Dias <freshman21@gmail.com>
2019-04-29 19:57:35 +0100

    feat: green screen

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -8,7 +8,7 @@ const snap = document.querySelector('.snap');
 function getVideo() {
     navigator.mediaDevices.getUserMedia({video: true, audio: false})
     .then(localMediaStream => {
-        console.log(localMediaStream);
+        //console.log(localMediaStream);
         video.srcObject = localMediaStream;
         video.play();
     }).catch(err => {
@@ -28,7 +28,9 @@ function paintToCanvas() {
         let pixels = ctx.getImageData(0, 0, width, height);
         //change pixels
         //pixels = redEffect(pixels);
-        pixels = rgbSplit(pixels);
+        // pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.1;
+        pixels = greenScreen(pixels);
         //put changed pixels in image
         ctx.putImageData(pixels, 0, 0);
     }, 16);
@@ -59,10 +61,37 @@ function redEffect(pixels) {
 
 function rgbSplit(pixels) {
     for (let i = 0; i < pixels.data.length; i += 4) {
-        pixels.data[i - 150] = pixels.data[i + 0] + 100; //R
-        pixels.data[i + 100] = pixels.data[i + 1] - 50; //G
-        pixels.data[i - 150] = pixels.data[i + 2] * 0.5; //B
+        pixels.data[i - 150] = pixels.data[i + 0]; //R
+        pixels.data[i + 500] = pixels.data[i + 1]; //G
+        pixels.data[i - 350] = pixels.data[i + 2]; //B
     }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+    });
+
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if (red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax) {
+            // take it out!
+            pixels.data[i + 3] = 0;
+        }
+    }
+
     return pixels;
 }
 
